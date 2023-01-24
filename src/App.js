3daff4b289b94ff030b4fd981052c9380af18926
hitesh19426 <hitesh19426@iiitd.ac.in>
2023-01-24 10:44:52 +0530

    added navbar

--- src/App.js
+++ src/App.js
@@ -1,53 +1,36 @@
 import "./App.css";
 import {
   BrowserRouter,
-  createBrowserRouter,
-  createRoutesFromElements,
   Route,
-  RouterProvider,
   Routes,
 } from "react-router-dom";
 import Users from "./pages/Users/Users";
 import React from "react";
-import Home from "./pages/Home";
+import Layout from "./pages/Home";
 import Places from "./pages/Places/Places";
 import Authenticate from "./pages/Users/Auth";
 import NewPlace from "./pages/Places/NewPlace";
 import UpdatePlace from "./pages/Places/UpdatePlace";
 import ErrorPage from "./pages/ErrorPage";
 
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route path="/" element={<Home />} errorElement={<ErrorPage />}>
-      <Route errorElement={<ErrorPage />}>
-        <Route index element={<Users />} />
-        <Route path="/:uid/places" element={<Places />} />
-        <Route path="/auth" element={<Authenticate />} />
-        <Route path="/places/new" element={<NewPlace />} />
-        <Route path="/places/:pid" element={<UpdatePlace />} />
-      </Route>
-    </Route>
-  )
-);
-
 function App() {
   return (
-    <RouterProvider router={router} />
+    // <RouterProvider router={router} />
 
     // alternative way
-    // <BrowserRouter>
-    //   <Routes>
-    //     <Route path="/" element={<Home />} errorElement={<ErrorPage />}>
-    //       <Route errorElement={<ErrorPage />}>
-    //         <Route index element={<Users />} />
-    //         <Route path="/:uid/places" element={<Places />} />
-    //         <Route path="/auth" element={<Authenticate />} />
-    //         <Route path="/places/new" element={<NewPlace />} />
-    //         <Route path="/places/:pid" element={<UpdatePlace />} />
-    //       </Route>
-    //     </Route>
-    //   </Routes>
-    // </BrowserRouter>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />} errorElement={<ErrorPage />}>
+          <Route errorElement={<ErrorPage />}>
+            <Route index element={<Users />} />
+            <Route path="/:uid/places" element={<Places />} />
+            <Route path="/auth" element={<Authenticate />} />
+            <Route path="/places/new" element={<NewPlace />} />
+            <Route path="/places/:pid" element={<UpdatePlace />} />
+          </Route>
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,21 +1,45 @@
 import React from 'react'
-import { Link, Outlet } from 'react-router-dom'
+import { Link, NavLink, Outlet } from 'react-router-dom'
+
+
+function NavBar() {
+    return (
+        <nav className="navbar navbar-expand-md bg-light" data-bs-theme="dark">
+            <div className="container-fluid">
+                <a className="navbar-brand" href="#">SpotShare</a>
+                <button className="navbar-toggler collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+                        <NavLink className="nav-link active" aria-current="page" to="/"> Users </NavLink>
+                        <NavLink className="nav-link active" aria-current="page" to="/auth"> Auth </NavLink>
+                        <NavLink className="nav-link active" aria-current="page" to="/places/new"> Create Place </NavLink>
+                        <NavLink className="nav-link active" aria-current="page" to="/u1/places"> Places </NavLink>
+                        <NavLink className="nav-link active" aria-current="page" to="/places/p1"> Edit Place </NavLink>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    )
+}
 
 export default function Home() {
-  return (
-    <div>
-        <header> Users Page header </header>
-        <nav>
-            <ul>
-                <li><Link to="/"> Home </Link> </li>
-                <li><Link to="/auth"> Auth </Link> </li>
-                <li><Link to="/places/new"> NewPlace </Link> </li>
-                <li><Link to="/u1/places"> Places </Link> </li>
-                <li><Link to="/places/p1"> Edit </Link> </li>
-            </ul>
-        </nav>
+    return (
+        <div>
+            <NavBar />
 
-        <Outlet />
-    </div>
-  )
+            {/* <nav>
+                <ul>
+                    <li><Link to="/"> Home </Link> </li>
+                    <li><Link to="/auth"> Auth </Link> </li>
+                    <li><Link to="/places/new"> NewPlace </Link> </li>
+                    <li><Link to="/u1/places"> Places </Link> </li>
+                    <li><Link to="/places/p1"> Edit </Link> </li>
+                </ul>
+            </nav> */}
+
+            <Outlet />
+        </div>
+    )
 }
